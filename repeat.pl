2e073c5b59cb07bbbd19f6d1546edfb106cf751f
Andres Cordova <andrescordova87@gmail.com>
2014-03-22 23:36:06 -0430

    Prints all words in file one by one

---
+++ repeat.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+
+#Basic file reading using perl
+
+my $file = $ARGV[0];
+my $opened = open(my $file_handler, "<", $file);
+
+if(!$opened){
+    die "Could not open file";
+}
+
+while(!eof($file_handler)){
+    my $line = readline($file_handler);
+    my @words = split(' ', $line);
+    for my $i (@words){
+	print " $i"
+    }
+}
+print "\n"
